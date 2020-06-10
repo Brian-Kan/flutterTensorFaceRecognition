64c26c6f86db830cbda76f919fd4dce2477f5f5b
Brian Kan <BrianKan00@hotmail.com>
2020-06-10 15:12:24 -0400

    Testing to see if I can rename root folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,11 @@
-
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
 import 'package:tflite/tflite.dart';
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MaterialApp(
-    theme: ThemeData(
-      primarySwatch: Colors.blue
-    ),
+    theme: ThemeData(primarySwatch: Colors.blue),
     home: HomePage(),
   ));
 }
@@ -23,11 +20,11 @@ class _HomePageState extends State<HomePage> {
   File _image;
   List _output;
 
- @override
+  @override
   void initState() {
     super.initState();
     _isLoading = true;
-    loadMLModel().then((value){
+    loadMLModel().then((value) {
       setState(() {
         _isLoading = false;
       });
@@ -37,79 +34,65 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Facial Recognition with Flutter")
-      ),
-      body: _isLoading ? Container(
-        alignment: Alignment.center,
-        child: CircularProgressIndicator(),
-      ) : Container(
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-
-            children: <Widget>[
-              _image == null ? Container():Image.file(_image),
-
-              _output == null ? Text(""): Text(
-                "${_output[0]["label"]}",
-                style: TextStyle(
-                  fontFamily: 'Comfortaa',
-                  fontSize: 20.0,
-                  color: Colors.blue
-                ),
-              )
-            ],
-            
-          )
-        )
-      ),
+      appBar: AppBar(title: Text("Facial Recognition with Flutter")),
+      body: _isLoading
+          ? Container(
+              alignment: Alignment.center,
+              child: CircularProgressIndicator(),
+            )
+          : Container(
+              child: SingleChildScrollView(
+                  child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                _image == null ? Container() : Image.file(_image),
+                _output == null
+                    ? Text("")
+                    : Text(
+                        "${_output[0]["label"]}",
+                        style: TextStyle(
+                            fontFamily: 'Comfortaa',
+                            fontSize: 20.0,
+                            color: Colors.blue),
+                      )
+              ],
+            ))),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
-          chooseImage();
-        },
-        child: Icon(
-          Icons.image
-        )
-      ),
+          onPressed: () {
+            chooseImage();
+          },
+          child: Icon(Icons.image)),
     );
-  }  
+  }
 
   // This function chooses an image.
-  chooseImage() async{
+  chooseImage() async {
     var image = await ImagePicker.pickImage(source: ImageSource.gallery);
-    if(image == null) return null;
-    setState((){
+    if (image == null) return null;
+    setState(() {
       _isLoading = true;
       _image = image;
     });
     runModelOnImage(image);
   }
 
-  runModelOnImage(File image) async{
+  runModelOnImage(File image) async {
     var output = await Tflite.runModelOnImage(
-      path: image.path,
-      numResults: 2,
-      imageMean: 127.5,
-      imageStd: 127.5,
-      threshold: 0.5
-    );
-    setState((){
+        path: image.path,
+        numResults: 2,
+        imageMean: 127.5,
+        imageStd: 127.5,
+        threshold: 0.5);
+    setState(() {
       _isLoading = false;
       _output = output;
     });
   }
 
   // This function loads the image.
-  loadMLModel() async{
+  loadMLModel() async {
     await Tflite.loadModel(
-      model: "assets/model_unquant.tflite",
-      labels: "assets/labels.txt"
-    );
+        model: "assets/model_unquant.tflite", labels: "assets/labels.txt");
   }
-
 }
-
-
-
